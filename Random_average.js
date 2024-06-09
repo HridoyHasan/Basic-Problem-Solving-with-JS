eb15ff5d0b509faeea0edebec26f322528b2182b
Quazi Mahabubul Hasan <hridoyhasan14@gmail.com>
2024-06-09 20:43:45 +0600

    Update Random_average.js

--- Random_average.js
+++ Random_average.js
@@ -1,11 +1,12 @@
 // Take some random numeric inputs and find their sum and average.
 
+
 let n = prompt("");
 let values = n.split("");
 let numb = values.map(Number);
 let sum = 0
 for (let i = 0; i < numb.length; i++) {
-  sum += numb[i];
+  sum += numb[i]; //here In the context of the loop, myNums[i] is a way to access elements in the array myNums using the index i.
 };
 let average = sum / numb.length;
 
